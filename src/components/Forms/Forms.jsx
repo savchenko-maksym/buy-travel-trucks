398a77d438dc6f951f42dd5db3360516dcd413fd
savchenko-maksym <maxim.savchenko91@gmail.com>
2025-09-11 20:50:33 +0300

    add searchMenu

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,5 +1,7 @@
 import { Form, Field, Formik } from "formik";
 import s from "./Forms.module.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Forms = () => {
   const initialValues = {
@@ -22,35 +24,40 @@ const Forms = () => {
         </span>
       </div>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form className={s.formBook}>
-          <Field
-            className={s.field}
-            type="text"
-            name="username"
-            placeholder="Name*"
-          />
-          <Field
-            className={s.field}
-            type="email"
-            name="email"
-            placeholder="Email*"
-          />
-          <Field
-            className={s.field}
-            type="date"
-            name="date"
-            placeholder="Booking date*"
-          />
-          <Field
-            className={s.fieldComment}
-            as="textarea"
-            name="comment"
-            placeholder="Comment"
-          />
-          <button className={s.btn} type="submit">
-            Send
-          </button>
-        </Form>
+        {({ setFieldValue, values }) => (
+          <Form className={s.formBook}>
+            <Field
+              className={s.field}
+              type="text"
+              name="username"
+              placeholder="Name*"
+              required
+            />
+            <Field
+              className={s.field}
+              type="email"
+              name="email"
+              placeholder="Email*"
+              required
+            />
+            <DatePicker
+              className={s.field}
+              placeholderText="Booking date*"
+              selected={values.date}
+              onChange={(date) => setFieldValue("date", date)}
+              required
+            />
+            <Field
+              className={s.fieldComment}
+              as="textarea"
+              name="comment"
+              placeholder="Comment"
+            />
+            <button className={s.btn} type="submit">
+              Send
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
--- src/components/SearchMenu/SearchMenu.jsx
+++ src/components/SearchMenu/SearchMenu.jsx
@@ -1,4 +1,42 @@
+import { Form, Field, Formik } from "formik";
+import AcIcon from "../../assets/images/icons/ac.svg?react";
+import BathroomIcon from "../../assets/images/icons/bathroom.svg?react";
+import KitchenIcon from "../../assets/images/icons/kitchen.svg?react";
+import TvIcon from "../../assets/images/icons/tv.svg?react";
+import RadioIcon from "../../assets/images/icons/radio.svg?react";
+import RefrigeratorIcon from "../../assets/images/icons/refrigeration.svg?react";
+import MicrowaveIcon from "../../assets/images/icons/microwave.svg?react";
+import GasIcon from "../../assets/images/icons/gas.svg?react";
+import WaterIcon from "../../assets/images/icons/water.svg?react";
+
+const EQUIPMENT_MAP = {
+  AC: { icon: <AcIcon />, label: () => "AC" },
+  bathroom: { icon: <BathroomIcon />, label: () => "Bathroom" },
+  kitchen: { icon: <KitchenIcon />, label: () => "Kitchen" },
+  TV: { icon: <TvIcon />, label: () => "TV" },
+  radio: { icon: <RadioIcon />, label: () => "Radio" },
+  refrigerator: { icon: <RefrigeratorIcon />, label: () => "Refrigerator" },
+  microwave: { icon: <MicrowaveIcon />, label: () => "Microwave" },
+  gas: { icon: <GasIcon />, label: () => "Gas" },
+  water: { icon: <WaterIcon />, label: () => "Water" },
+};
+
+const VEHICLE_TYPES = {
+  fullyIntegrated: {},
+  alcove: {},
+  panelTruck: {},
+};
+
 const SearchMenu = () => {
-  return <h2>SearchMenu</h2>;
+  return (
+    <div>
+      <Formik>
+        <Form>
+          <Field name="location" />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 export default SearchMenu;
