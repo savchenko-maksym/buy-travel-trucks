6c680838c79614a83a036f37410d42452d6aa90e
savchenko-maksym <maxim.savchenko91@gmail.com>
2025-09-11 22:29:51 +0300

    seachForm

--- src/components/SearchMenu/SearchMenu.jsx
+++ src/components/SearchMenu/SearchMenu.jsx
@@ -8,6 +8,11 @@ import RefrigeratorIcon from "../../assets/images/icons/refrigeration.svg?react"
 import MicrowaveIcon from "../../assets/images/icons/microwave.svg?react";
 import GasIcon from "../../assets/images/icons/gas.svg?react";
 import WaterIcon from "../../assets/images/icons/water.svg?react";
+import Van from "../../assets/images/icons/Van.svg?react";
+import FullInt from "../../assets/images/icons/FullInt.svg?react";
+import Alcove from "../../assets/images/icons/Alcove.svg?react";
+import Loation from "../../assets/images/icons/Location.svg?react";
+import { useState } from "react";
 
 const EQUIPMENT_MAP = {
   AC: { icon: <AcIcon />, label: () => "AC" },
@@ -22,17 +27,56 @@ const EQUIPMENT_MAP = {
 };
 
 const VEHICLE_TYPES = {
-  fullyIntegrated: {},
-  alcove: {},
-  panelTruck: {},
+  fullyIntegrated: { icon: <FullInt />, label: () => "Fully Integrated" },
+  alcove: { icon: <Alcove />, label: () => "Alcove" },
+  panelTruck: { icon: <Van />, label: () => "Van" },
 };
 
-const SearchMenu = () => {
+const SearchMenu = ({ onSearch }) => {
+  const [selectedEquipment, setSelectedEquipment] = useState([]);
+  const [selectedVehicleTypes, setSelectedVehicleTypes] = useState([]);
+
+  const toggleEquipment = (equip) => {
+    setSelectedEquipment((prev) =>
+      prev.includes(equip) ? prev.filter((e) => e !== equip) : [...prev, equip]
+    );
+  };
+
+  const toggleVehicleType = (type) => {
+    setSelectedVehicleTypes((prev) =>
+      prev.includes(type) ? prev.filter((e) => e !== type) : [...prev, type]
+    );
+  };
+
+  const resetFilters = (resetForm) => {
+    resetForm(),
+      setSelectedEquipment([]),
+      setSelectedVehicleTypes([]),
+      onSearch({ location: "", equipment: [], vehicleTypes: [] });
+  };
+
+  const initialValues = {
+    location: "",
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    onSearch({
+      ...values,
+      equipment: selectedEquipment,
+      vehicleTypes: setSelectedVehicleTypes,
+    });
+    resetForm();
+  };
+
   return (
     <div>
-      <Formik>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form>
-          <Field name="location" />
+          <div>
+            <label>Location</label>
+            <Field name="location" placeholder="City" />
+          </div>
+
           <button type="submit">Search</button>
         </Form>
       </Formik>
