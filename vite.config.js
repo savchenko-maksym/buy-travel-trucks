914be4d69759730db4ec7e8e8def0e025532e773
savchenko-maksym <maxim.savchenko91@gmail.com>
2025-09-27 01:01:53 +0300

    add settings for vite

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [
-    svgr({
-      exportAsDefault: true, // ⚡ увімкне підтримку імпорту через ?react
-    }),
-    react(),
-  ],
+  plugins: [react(), svgr()],
   build: {
     sourcemap: true,
   },
